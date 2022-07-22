65e9053ddf623e061386d4cbfc1eb7fe861560c4
Simon Tscholl <35654158+sitscholl@users.noreply.github.com>
2022-07-22 17:21:38 +0200

    Update _functions.py

--- _functions.py
+++ _functions.py
@@ -3,6 +3,87 @@ import warnings
 from collections import defaultdict
 import pandas as pd
 from notion_client import Client
+import gspread
+
+
+import datetime
+woid = "89e88a749933453a827538bd7c7da0fd"
+token = 'secret_XDjIC2asAGztVnJ27vq0A6CraBTdpZgKISRxyGqOXjt'
+wo_date = datetime.date.today()
+
+#include workout notes and rating num as arguments
+def push_notion(token, log_id, wo_id, data, wo_date):
+    
+    notion = Client(auth=token)
+    
+    if not isinstance(wo_date, str):
+        wo_date = wo_date.strftime('%Y-%m-%d')
+    
+    #Check if workout entry exists
+    wo_row = call_notion(token, wo_id, query_filter = {'property': 'Date', 'date': {'equals': wo_date}})
+    
+    if len(wo_row['results']) == 0:
+        #Create workout entry
+        properties = {
+            "Name": {"title": [{"text": {"content": 'Strength'}}]},
+            "Date": {"date": {"start": wo_date}}
+            #"Notes": {'rich_text':[{'type': 'text', 'text': {'content': workout_notes}}]}
+            }
+        
+        workout_push = notion.pages.create(parent={"database_id": wo_id}, properties=properties)
+        wo_page_id = workout_push['id']
+        
+    else:
+        wo_page_id = wo_row['results'][0]['id']
+    
+    data_fill = data.copy()
+    for c in data.select_dtypes('number'):
+        data_fill[c] = data_fill[c].fillna(0).astype(float)
+          
+    #push exercises to exercise_log
+    for i in data_fill.index:
+        
+        row = data_fill.loc[i]
+        # Create a new page in notion
+        properties = {
+            "Name": {"title": [{"text": {"content": row['Exercise']}}]},
+            "Set": {"type": "number", "number": row['Set']},
+            "Weight": {"type": "number", "number": row['Weight']},
+            "Distance": {"type": "number", "number": row['Distance']},
+            "Reps": {"type": "number", "number": row['Reps']},
+            "RPE": {"type": "number", "number": row['RPE']},
+            "Order": {"type": "number", "number": row['Order']},
+            "Rest": {"type": "number", "number": row['Rest']},
+            "Notes": {'rich_text':[{'type': 'text', 'text': {'content': row['Notes']}}]},
+            "Failure": {'checkbox': bool(row['Failure'])},
+            "Exercise": {
+                "relation": [{"id": row['page_id']}]
+                },
+            "Workout": {
+                "relation": [{'id': wo_page_id}]
+                }
+        }
+    
+        log_push = notion.pages.create(parent={"database_id": log_id}, properties=properties)
+        
+# def push_gsheet(creds, log_id):
+    
+#     #Save backup to google sheets
+#     scope = ["https://spreadsheets.google.com/feeds",'https://www.googleapis.com/auth/spreadsheets',"https://www.googleapis.com/auth/drive.file","https://www.googleapis.com/auth/drive"]
+#     client = gspread.service_account_from_dict(creds, scope)
+#     sheet = client.open("Wo Backup").sheet1
+#     #gcols = sheet.row_values(1)
+    
+#     tbl_full = call_notion(token, log_id) #Download entire dataset
+
+#     for c in tbl_full.select_dtypes(np.number).columns:
+#         tbl_full[c] = tbl_full[c].fillna(0).astype(float)
+        
+#     sheet.clear()
+         
+#     tbl_full.drop('Mesocycle', axis = 1, inplace = True, errors = 'ignore') #Fix values of mesocycle
+#     sheet.append_rows([tbl_full.columns.tolist()]) #Add column titles
+#     sheet.append_rows(tbl_full.values.tolist(), value_input_option="USER_ENTERED") #Add data
 
 def call_notion(token, dbid, query_filter = None):
     
