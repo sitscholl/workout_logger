3ba6bb780986b8a5a95c16d7b655d0c778a5e7ae
Simon Tscholl <35654158+sitscholl@users.noreply.github.com>
2022-07-23 15:00:20 +0200

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -44,6 +44,7 @@ def get_notion(token, db_id, query_filter = None):
     db_raw = call_notion(token, db_id, query_filter)
     return(to_df(db_raw['results']))
 
+# --- Import Datasets ---
 ##Use Parent Exercise column (relation) to get parent exercise name instead of Parent column
 ex_database = get_notion(token, exercises_id)[['Parent', 'Name', 'Level', 'Type', 'Group', 'Group 2', 'Category', 'Muscles', 'Status', 'page_id']]
 ex_database = ex_database.sort_values(['Parent', 'Level', 'Name']).reset_index(drop = True)
@@ -59,6 +60,7 @@ ex_log = ex_log.sort_values(['Date', 'Exercise Name', 'Set']).reset_index(drop =
 active_exercises = ex_database.loc[ex_database['Status'].isin(['In Progress']), 'Name'].tolist()
 accessory_exercises = ex_database.loc[ex_database['Status'].isin(['Accessory']), 'Name'].tolist()
 
+# --- Initialize persistent variables ---
 if "sets" not in st.session_state.keys():
     set_dict = {i: 1 for i in ex_database['Name'].unique()}
     st.session_state.sets = set_dict
@@ -73,25 +75,19 @@ if "defaults" not in st.session_state.keys():
 if "end_time" not in st.session_state.keys():
     st.session_state.end_time = None
     
-#### App layout
+wo_tbl_cols = ['Order', 'Exercise', 'Set', 'Weight', 'Distance', 'Reps', 'RPE', 'Failure', 'Notes']
+if "wo_tbl" not in st.session_state.keys():
+    st.session_state.wo_tbl = pd.DataFrame(columns = wo_tbl_cols)
+    
+# --- App Layout ---
 st.title('Workout Logger')
-
-st.write(st.session_state.order)
        
 c1, c2 = st.columns([1, 4])
 with c1:
     with st.expander('Change Date'):
         wo_date = st.date_input('Workout Date', value = date.today())
-        
-wo_tbl_cols = ['Order', 'Exercise Name', 'Set', 'Weight', 'Distance', 'Reps', 'RPE', 'Failure', 'Notes']
-wo_tbl = call_notion(token, log_id, query_filter = {'property': 'Date', 'rollup': {'date': {'equals': datetime.datetime.strftime(wo_date, '%Y-%m-%d')}}})
-
-if len(wo_tbl['results']) == 0:
-    wo_tbl = pd.DataFrame(columns = wo_tbl_cols)
-else:
-    wo_tbl = to_df(wo_tbl['results'])[wo_tbl_cols]
-    wo_tbl.sort_values('Order', inplace = True)
-          
+            
+# --- Data Input Form ---    
 ex = st.selectbox('Exercise', options = active_exercises + accessory_exercises + [i for i in ex_database['Name'].unique() if i not in active_exercises])
 with st.form(ex):
         
@@ -102,19 +98,19 @@ with st.form(ex):
     c1, c2 = st.columns(2)
     with c1:
         weight = st.number_input('Weight', value = st.session_state.defaults[ex]['Weight'], step = .5)
+        distance = st.number_input('Distance', value = st.session_state.defaults[ex]['Distance'], step = .5)
         reps = st.number_input('Reps', value = st.session_state.defaults[ex]['Reps'], step = 1.0)
-        failure = st.checkbox('To Failure?')
-        
-        st.write("")
-        st.write("")
-        timer = st.time_input('Timer', value = time(2, 30))
+        RPE = st.number_input('RPE', value = st.session_state.defaults[ex]['RPE'], min_value = 0.0, max_value = 10.0, step = .5)        
         
     with c2:
-        distance = st.number_input('Distance', value = st.session_state.defaults[ex]['Distance'], step = .5)
-        RPE = st.number_input('RPE', value = st.session_state.defaults[ex]['RPE'], min_value = 0.0, max_value = 10.0, step = .5)
-        notes = st.text_input('Notes')
+        st.write('')
+        st.write('')
+        st.write('')
         
-        st.write("")
+        failure = st.checkbox('To Failure?')
+        notes = st.text_input('Notes')
+        timer = st.time_input('Timer', value = time(2, 30))
+        
         submitted = st.form_submit_button('Submit')
     
     if submitted:
@@ -129,8 +125,7 @@ with st.form(ex):
         st.session_state.defaults[ex].update({'Weight': weight, 'Distance': distance, 
                                               'Reps': reps, 'RPE': RPE})
         
-        push_notion(token = token, log_id = log_id, wo_id = workouts_id, 
-                    data = data_new, wo_date = wo_date)
+        st.session_state.wo_tbl = pd.concat([st.session_state.wo_tbl, data_new[wo_tbl_cols]])
         
         st.session_state.sets[ex] += 1
         st.session_state.order += 1
@@ -143,13 +138,17 @@ stop = st.button('Stop timer')
 
 if stop:
     st.session_state.end_time = None
+    
+st.markdown('---')
+
+# --- Display Summary Statistics ---
 
 col1, col2 = st.columns(2)
 agg_funcs = {'Set': lambda x: len(x), 'Reps': np.sum, 'RPE': np.mean}
 with col1:
     st.markdown('### This Workout')
     st.caption(datetime.datetime.strftime(wo_date, '%Y-%m-%d'))
-    st.table(wo_tbl.groupby('Exercise Name')['Set', 'Reps', 'RPE'].agg(agg_funcs).style.format(precision=1))
+    st.table(st.session_state.wo_tbl.groupby('Exercise')['Set', 'Reps', 'RPE'].agg(agg_funcs).style.format(precision=1))
     
 with col2:
     last_wo_date = ex_log.loc[ex_log['Date'].dt.date != wo_date, 'Date'].max()
@@ -159,6 +158,8 @@ with col2:
     last_wo = ex_log.loc[ex_log['Date'] == last_wo_date, ['Exercise Name', 'Set', 'Weight', 'Distance', 'Reps', 'RPE', 'Failure', 'Notes']]
     st.table(last_wo.groupby('Exercise Name')['Set', 'Reps', 'RPE'].agg(agg_funcs).style.format(precision=1))
     
+# --- Display Summary Chart ---
+
 log_agg = ex_log.groupby(['Date', 'Exercise Name'], as_index = False)['Reps'].sum()
 
 c = alt.Chart(log_agg).mark_line(point=True).encode(
@@ -170,15 +171,33 @@ st.altair_chart(c, use_container_width=True)
 
 st.markdown('---')
 
-workout_notes = st.text_input('Workout Notes:')  
+# --- Workout Level Input ---
+
+c1, c2 = st.columns(2)
+workout_notes = c1.text_input('Workout Notes:') 
+workout_rating = c2.slider('Workout Rating', min_value = 0, max_value = 10, step = 1)
+
+# --- Push Data to Notion ---
+
+end_wo = st.button('Finish Workout')
+if end_wo:
+        push_notion(token = token, log_id = log_id, wo_id = workouts_id, 
+                    data = data_new, wo_date = wo_date, wo_notes = workout_notes,
+                    wo_rating = workout_rating)
+        
+        st.balloons() 
 
 st.markdown('---')
 
+# --- Detailed Tables ---
+
 with st.expander('Check workout log'):
-    st.dataframe(wo_tbl)
+    st.dataframe(st.session_state.wo_tbl)
     
 with st.expander('Check last workout'):
     st.dataframe(last_wo)
+      
+# --- Timer ---
     
 if (st.session_state.end_time != None) and (st.session_state.end_time > datetime.datetime.now()):
     #Get remaining seconds to scheduled end time
