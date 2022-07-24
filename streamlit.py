360d1d6faee9ddb172c45849cbd9dc3707652f08
Simon Tscholl <35654158+sitscholl@users.noreply.github.com>
2022-07-24 11:44:10 +0200

    improved st.metric

--- streamlit.py
+++ streamlit.py
@@ -176,7 +176,7 @@ for nam, col in zip(compare.index, cols):
         delta = compare.loc[nam, 'Reps'] - compare.loc[nam, 'Reps_last']
         
         val = val if val == val else 0
-        delta = delta if delta == delta else 0
+        delta = delta if delta == delta else None
         st.metric(label = nam, value = val, delta = delta)
 
 col1, col2 = st.columns(2)
