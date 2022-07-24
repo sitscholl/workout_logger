cba0c6a9c4370abb5c7bb42caab42e1b4cb92d53
Simon Tscholl <35654158+sitscholl@users.noreply.github.com>
2022-07-24 12:10:07 +0200

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -59,6 +59,7 @@ ex_database = get_notion(token, exercises_id)[['Parent', 'Name', 'Level', 'Type'
 ex_database = ex_database.sort_values(['Parent', 'Level', 'Name']).reset_index(drop = True)
 
 ex_log = get_notion(token, log_id, query_filter = {'property': 'Date', 'rollup': {'date': {'past_month': {}}}})
+ex_log = ex_log.copy()
 ex_log['Date'] = pd.to_datetime(ex_log['Date'], format = '%Y-%m-%d')
 ex_log.sort_values(['Date', 'Exercise Name'], inplace = True)
 ex_log['Set_fill'] = ex_log.groupby(['Date', 'Exercise Name'])['Set'].cumcount()+1
