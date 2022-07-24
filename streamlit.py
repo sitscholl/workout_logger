07c150b1c2a9ed80df3bf95c7ddce9ab3e792d66
Simon Tscholl <35654158+sitscholl@users.noreply.github.com>
2022-07-24 14:18:14 +0200

    fix bar_chart

--- streamlit.py
+++ streamlit.py
@@ -199,7 +199,7 @@ st.pyplot(fig)
 
 test = log_agg.loc[log_agg['Exercise Name'] == ex]
 st.dataframe(test)
-st.bar_chart(test[['Date', 'Reps']])
+st.bar_chart(test[['Date', 'Reps']].set_index('Date')
 
 st.markdown('---')
 
