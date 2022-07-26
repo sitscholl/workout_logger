bcd5e781cff07f7252ffd3e7af55851752fe17ed
Simon Tscholl <35654158+sitscholl@users.noreply.github.com>
2022-07-26 16:58:53 +0200

    fix callback

--- streamlit.py
+++ streamlit.py
@@ -97,7 +97,7 @@ with c1:
     wo_date = st.date_input('Workout Date', value = date.today())
 with c2:
     bw_default = bodyweight[0] if bodyweight[0] == bodyweight[0] else np.nan
-    bw = st.number_input('Bodyweight', value = bw_default, step = 1.0, on_change = get_bodyweight, args = (bw))
+    bw = st.number_input('Bodyweight', value = bw_default, step = 1.0, on_change = get_bodyweight, kwargs = (dict(x = bw))
         
 last_wo_date = ex_log.loc[(ex_log['Date'].dt.date != wo_date) & (ex_log['Category'] == 'Strength'), 'Date'].max()
 last_wo = ex_log.loc[ex_log['Date'] == last_wo_date, wo_tbl_cols]
