39dbb44acb9e6f495abf4ab31f7274535025d0f9
Simon Tscholl <35654158+sitscholl@users.noreply.github.com>
2022-08-05 19:17:48 +0200

    removed default exercise selection

--- streamlit.py
+++ streamlit.py
@@ -108,11 +108,11 @@ last_wo = ex_log.loc[ex_log['Date'] == last_wo_date, wo_tbl_cols]
 # --- Data Input Form ---  
 
 norder = np.sum([len(i) for i in mutable.values()])+1
-ex_default = last_wo.loc[last_wo['Order'].fillna(999).astype(int) == norder, 'Exercise Name'].tolist()[0]
+#ex_default = last_wo.loc[last_wo['Order'].fillna(999).astype(int) == norder, 'Exercise Name'].tolist()[0] #If exercise not found, this raises an Index Error
 ex_options = active_exercises + accessory_exercises + [i for i in ex_database['Name'].unique() if i not in active_exercises]
 ex = st.selectbox('Exercise', 
-                  options = ex_options,
-                  index = ex_options.index(ex_default))
+                  options = ex_options)
+                  #index = ex_options.index(ex_default))
 with st.form(ex):
         
     nset = len(mutable[ex])+1
