3827f1710a66c0a1e09d117c9d4c0c8872bfee9c
Simon Tscholl <35654158+sitscholl@users.noreply.github.com>
2023-01-27 08:02:22 +0100

    testing sound output

--- streamlit.py
+++ streamlit.py
@@ -257,5 +257,16 @@ if (end_time[0] != None) and (end_time[0] > datetime.datetime.now()):
     
     asyncio.run(start_timer(ph, t))
     
+    html_string = """
+            <audio controls autoplay>
+              <source src="https://www.orangefreesounds.com/wp-content/uploads/2022/04/Small-bell-ringing-short-sound-effect.mp3" type="audio/mp3">
+            </audio>
+            """
+
+    sound = st.empty()
+    sound.markdown(html_string, unsafe_allow_html=True)  # will display a st.audio with the sound you specified in the "src" of the html_string and autoplay it
+    time.sleep(2)  # wait for 2 seconds to finish the playing of the audio
+    sound.empty()  # optionally delete the element afterwards
+    
     ph.empty()
     end_time[0] = None
