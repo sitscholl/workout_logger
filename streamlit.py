f1c898f0c2fc776aee16964bffdbd7670617f61a
Simon Tscholl <35654158+sitscholl@users.noreply.github.com>
2023-01-28 14:42:12 +0100

    change heading

--- streamlit.py
+++ streamlit.py
@@ -176,12 +176,10 @@ agg_funcs = {'Set': lambda x: len(x), 'Reps': np.sum, 'RPE': np.mean}
 
 wo_agg = wo_tbl.groupby('Exercise Name')[['Set', 'Reps', 'RPE']].agg(agg_funcs)
 
-st.markdown('### This Workout')
+st.markdown('### Workout Overview')
 st.caption(datetime.datetime.strftime(wo_date, '%Y-%m-%d'))
 st.table(wo_agg.style.format(precision=1))
     
-st.markdown('---')
-
 # --- Detailed Tables ---
 
 with st.expander('Check detailed workout log'):
